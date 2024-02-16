5c272c7206c9dcf42ef7473ff6016771dd879f5f
Marshall Hotaling <marshall.hotaling@gmail.com>
2024-02-16 14:50:36 -0700

    finished if

--- ifStatements.js
+++ ifStatements.js
@@ -9,9 +9,11 @@
 // Experiment with manipulating the value held within numQuarters to make sure both conditions can be achieved.
 
 var numQuarters = 0;
+if (numQuarters >= 50){
+    console.log("I have enough money for a gumball");
+} else{console.log("I don't have enough money for a gumball");
+} 
 
-console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
 
 
 // CHALLENGE 2
@@ -28,4 +30,12 @@ console.log("I don't have enough money for a gumball");
 
 var cupsOfFlour = 1;
 var hasSauce = true;
-
+if (cupsOfFlour === 1 && hasSauce === true) {
+    console.log("I cannot make pizza");
+} else if (cupsOfFlour === 5 && hasSauce === false ) {
+    console.log("I cannot make pizza")
+} else if (cupsOfFlour === 2 && hasSauce === true ){
+    console.log ("I can make pizza")
+}else if (cupsOfFlour === 3 && hasSauce === true){
+    confirm.log ("I can make pizza")
+}
